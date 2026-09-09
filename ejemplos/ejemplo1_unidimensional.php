e15355ff1ad0574cc7d44c3e9c1470a546a594e2
estjuanantoniomoyaob-web <est.juanantonio.moya.ob@unifranz.edu.bo>
2026-09-09 09:02:51 -0400

    Update ejemplo1_unidimensional.php

--- ejemplos/ejemplo1_unidimensional.php
+++ ejemplos/ejemplo1_unidimensional.php
@@ -4,30 +4,25 @@
 
 $numeros = [10, 20, 30, 40, 50];
 
-$buscar = 30;
-
-$encontrado = false;
-
 echo "Arreglo: ";
 print_r($numeros);
 
-echo "Buscando el número: $buscar\n\n";
+$buscar = readline("Ingrese el número que desea buscar: ");
 
-// Búsqueda lineal
+$encontrado = false;
+
 for ($i = 0; $i < count($numeros); $i++) {
 
     if ($numeros[$i] == $buscar) {
-
         echo "Elemento encontrado: $buscar\n";
         echo "Posición: $i\n";
-
         $encontrado = true;
         break;
     }
 }
 
 if (!$encontrado) {
-    echo "Elemento no encontrado\n";
+    echo "El elemento $buscar no se encuentra en el arreglo.\n";
 }
 
 ?>
